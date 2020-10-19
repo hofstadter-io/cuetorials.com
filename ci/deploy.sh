0a4ecc30a5d1d5ae6d26131c596820cc0b4f7893
Tony Worm <tony@hofstadter.io>
2020-10-19 19:28:22 -0400

    fix up some hostname / param bugs

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,12 +4,12 @@ set -euo pipefail
 PROJECT=hof-io--develop
 TAG=$(git rev-parse --short HEAD | tr -d "\n")
 
-hugo --baseURL https://cuetorials.com -d dist
+hugo --baseURL https://cuetorials.com/ -d dist
 
 docker build -t us.gcr.io/$PROJECT/cuetorials.com:manual .
 docker push us.gcr.io/$PROJECT/cuetorials.com:manual
 
-rm -rf dist
+# rm -rf dist
 
 kubectl get pods -n websites
 
