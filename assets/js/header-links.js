48c01c2fbf984fce8990e19fe6dc443ffd98dbac
Tony Worm <tony@hofstadter.io>
2021-09-28 02:16:34 -0400

    add link icons to headers
    Signed-off-by: Tony Worm <tony@hofstadter.io>

---
+++ assets/js/header-links.js
@@ -0,0 +1,16 @@
+function addHeaderLinks() {
+  var content = document.getElementById("main-content");
+  var h3s = content.querySelectorAll("h3");
+
+  h3s.forEach(h3 => {
+    if (h3.parentNode == content) {
+      console.log(h3)
+      var a = document.createElement("a");
+      a.className = "ml-2 fas fa-link fa-xs";
+      a.href = window.location.pathname + "#" + h3.id
+      h3.appendChild(a);
+    }
+  })
+}
+
+addHeaderLinks();
