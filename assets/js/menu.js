ce7c5a6e28ea06753577e6e0b4d8e56132f62787
Tony Worm <tony@hofstadter.io>
2023-01-08 00:30:56 -0500

    update menu to have larger clickable area for menu collapsing

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,20 +1,22 @@
 window.toggleMenuItem = function (e) {
-	e = e || window.event;
+  e = e || window.event;
   var src = e.target || e.srcElement;
-	console.log(e)
 
-	children = src.parentNode.nextElementSibling;
-	console.log(children)
+  if (src.nodeName === "SPAN") {
+    src = src.children[0]
+  }
+  p1 = src.parentNode;
+  p2 = p1.parentNode;
 
-	classes = src.className.split(" ");
-	var open = classes.indexOf("fa-caret-down") >= 0
-	if (open) {
-		src.className = src.className.replace("down", "right")
-		children.className += " d-none"
-	} else {
-		src.className = src.className.replace("right", "down")
-		children.className = children.className.replace(" d-none", "")
-	}
+  children = p2.nextElementSibling;
 
-
+  classes = src.className.split(" ");
+  var open = classes.indexOf("fa-caret-down") >= 0
+  if (open) {
+    src.className = src.className.replace("down", "right")
+    children.className += " d-none"
+  } else {
+    src.className = src.className.replace("right", "down")
+    children.className = children.className.replace(" d-none", "")
+  }
 }
